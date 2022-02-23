8b85e05995d3c465284b964babbe16aa298d651c
qwwerty <rhalfsouzaa@gmail.com>
2022-02-22 22:48:35 -0300

    terminated

---
+++ components/atoms/StardCycle.vue
@@ -0,0 +1,22 @@
+<template functional>
+	<section
+		class="flex flex-col items-center justify-center flex-1 p-10 md:p-16"
+	>
+		<h1 class="text-2xl font-medium text-center">
+			Start a new cycle to receive new challenges
+		</h1>
+		<img
+			src="icons/level-up.svg"
+			alt="Level up"
+			class="mt-10 md:mt-16"
+			:style="{ minHeight: '70px' }"
+		/>
+		<p class="mt-4 text-center text-md">
+			Increase your level by completing challenges
+		</p>
+	</section>
+</template>
+
+<script lang="ts"></script>
+
+<style></style>
---
+++ components/organisms/Card.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="flex flex-1 w-full mt-8 bg-white rounded-md lg:mt-0 shadow-sm">
+		<Challenge v-if="currentChallenge" v-bind="currentChallenge" />
+		<StartCycle v-else />
+		<LevelUpModel />
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { mapGetters } from "vuex"
+
+import Challenge from "~/components/molecules/Challenge.vue";
+import StartCycle from "~/components/atoms/StardCycle.vue";
+import LevelUpModel from "~/components/atoms/LevelUpModal.vue";
+
+export default Vue.extend({
+	components: {
+		Challenge,
+		StartCycle,
+		LevelUpModel,
+	},
+	computed: mapGetters('Challenges', ['currentChallenge'])
+});
+</script>
